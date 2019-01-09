3c0b1b128d8d50091e8fdfb5714f655f0a73de1d
Leszek Krupiński <leafnode@gmail.com>
2019-01-09 15:13:08 +0100

    Change syntax of env vars definition

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,8 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_env_vars_added(host):
+    f = host.file('/etc/environment')
 
     assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert f.contains("VARIABLE='variable value'")
